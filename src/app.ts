0022940f1de8e83f4fd80e029e4ce5bf5788a7ba
iskandarzero <riderzerotg@gmail.com>
2022-09-14 15:36:43 -0300

    feat: adds productRoute in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import productRoute from './routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/products', productRoute);
 
 export default app;
