302d9f218c90835f59b6c706a0f279480750da4d
iskandarzero <riderzerotg@gmail.com>
2022-09-14 18:27:25 -0300

    refactor: declare create as arroww function in user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,16 @@
+import UserModel from '../models/user.model';
+import connection from '../models/connection';
+import generateToken from '../auth/generateToken';
+import Token from '../interfaces/token.interface';
+import User from '../interfaces/user.interface';
+
+export default class UserService {
+  constructor(private model = new UserModel(connection)) {}
+
+  public async create(user: User): Promise<Token> {
+    await this.model.create(user);
+    const token = await generateToken(user.username);
+    
+    return token;
+  }
+}
