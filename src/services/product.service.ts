50498546db3750a9f0b14e9863cb3f5ab4b6f1cb
iskandarzero <riderzerotg@gmail.com>
2022-09-14 16:24:27 -0300

    fix: product service constructor

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -3,11 +3,7 @@ import ProductsModel from '../models/product.model';
 import Product from '../interfaces/product.interface';
 
 export default class ProductService {
-  public model: ProductsModel;
-
-  constructor() {
-    this.model = new ProductsModel(connection);
-  }
+  constructor(private model = new ProductsModel(connection)) {}
 
   public async create(name: string, amount: string): Promise<Product> {
     const product = await this.model.create(name, amount);
