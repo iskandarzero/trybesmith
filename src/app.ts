31cc7c4b00bf5f4c7e8eba4b7064909f1876cc9d
iskandarzero <riderzerotg@gmail.com>
2022-09-15 07:26:44 -0300

    feat: order routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import productRoute from './routes/products.routes';
 import userRoute from './routes/user.routes';
+import orderRoute from './routes/order.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRoute);
 app.use('/users', userRoute);
+app.use('/orders', orderRoute);
 
 export default app;
