1c32dd9806f6da3913a0f1c85f535a096d991b2f
iskandarzero <riderzerotg@gmail.com>
2022-09-17 03:53:17 -0300

    feat: adds login rounte in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import productRoute from './routes/products.routes';
 import userRoute from './routes/user.routes';
 import orderRoute from './routes/order.routes';
+import loginRoute from './routes/login.routes';
+
+import validateLogin from './middlewares/login.middleware';
 
 const app = express();
 
@@ -9,5 +12,6 @@ app.use(express.json());
 app.use('/products', productRoute);
 app.use('/users', userRoute);
 app.use('/orders', orderRoute);
+app.use('/login', validateLogin, loginRoute);
 
 export default app;
