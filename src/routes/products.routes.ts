eba26515dd8380138ae1b24da91704fa4f4e3ce5
iskandarzero <riderzerotg@gmail.com>
2022-09-17 04:12:23 -0300

    feat: adds middleware to validate name and amount

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import { validateName, validateAmount } from '../middlewares/product.middleware';
 
 const productRoute = Router();
 
 const productController = new ProductController();
 
-productRoute.post('/', productController.create);
+productRoute.post('/', validateName, validateAmount, productController.create);
 productRoute.get('/', productController.getAll);
 
 export default productRoute;
