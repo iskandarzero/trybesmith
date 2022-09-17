a4af1c9f9611078f3f06f323748aa56494e31d43
iskandarzero <riderzerotg@gmail.com>
2022-09-17 03:52:23 -0300

    feat: adds login service

---
+++ src/services/login.service.ts
@@ -0,0 +1,21 @@
+import LoginModel from '../models/login.model';
+import connection from '../models/connection';
+import Token from '../interfaces/token.interface';
+import generateToken from '../auth/generateToken';
+import Login from '../interfaces/login.interface';
+
+export default class LoginService {
+  constructor(private loginModel = new LoginModel(connection)) {}
+
+  public async login(user: Login): Promise<Token | null> {
+    const logged = await this.loginModel.login(user);
+    
+    if (logged) {
+      const token = generateToken(user.username);
+
+      return token;
+    }
+    
+    return null;
+  }
+}
