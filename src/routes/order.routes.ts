5b9b4c259373ef4c3ab7f682242eeb6cd8ae5ea3
iskandarzero <riderzerotg@gmail.com>
2022-10-12 04:40:48 -0300

    feat: order route

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import OrderController from '../controllers/order.controller';
+import validateOrder from '../middlewares/order.middleware';
+import validateToken from '../middlewares/token.middleware';
 
 const orderRoute = Router();
 
 const orderController = new OrderController();
 
 orderRoute.get('/', orderController.getAll);
+orderRoute.post('/', validateToken, validateOrder, orderController.create);
 
 export default orderRoute;
